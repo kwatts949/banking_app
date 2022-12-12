b4f6818db88024d3ded1cbf20d3fdfec681f1a16
Kayleigh Watts <kwatts949@gmail.com>
2022-12-12 14:35:13 +0000

    Add method to convert integer to 2 d.p & passing test

--- lib/statement.rb
+++ lib/statement.rb
@@ -6,7 +6,7 @@ class Statement
   def display(transactions)
     print_header
     if transactions.empty?
-      p "  ||  ||  || #{@balance}"
+      p "  ||  ||  || #{convert(@balance)}"
     else
       print_transactions(transactions)
     end
@@ -16,12 +16,16 @@ class Statement
     p transactions
     transactions.map do |transaction|
       if transaction.credit == 'true'
-        p "#{transaction.date} || #{transaction.amount} ||  || #{@balance}"
+        p "#{transaction.date} || #{transaction.amount} ||  || #{convert(@balance)}"
       end
     end
   end
 
   def print_header
     p "date || credit || debit || balance"
+  end
+
+  def convert(value)
+    return sprintf('%.2f', value)
   end
 end

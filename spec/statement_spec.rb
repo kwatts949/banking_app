b8f692f67e3fcb901d53014e93ac184dad5598fe
Kayleigh Watts <kwatts949@gmail.com>
2022-12-12 15:07:16 +0000

    Add passing tests for statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,27 +1,49 @@
 require 'statement'
 
 RSpec.describe 'Statement' do
+
   it 'prints an empty statement to the console' do
     statement = Statement.new
     expect do
       statement.display([])
     end.to output("\"date || credit || debit || balance\"\n\"  ||  ||  || 0.00\"\n").to_stdout
+    end
+
+  context 'deposits' do
+    it 'prints a deposit transaction to the console' do
+      statement = Statement.new
+      fake_deposit = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
+      expect do
+        statement.display([fake_deposit])
+      end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n").to_stdout
+    end
+
+    it 'prints two deposit transactions to the console and shows the correct balance' do
+      statement = Statement.new
+      fake_deposit_1 = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
+      fake_deposit_2 = double(:deposit, amount: 800, date: "02-12-2022", credit: 'true')
+      expect do
+        statement.display([fake_deposit_1, fake_deposit_2])
+      end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n\"02-12-2022 || 800.00 ||  || 1300.00\"\n").to_stdout
+    end
   end
 
-  it 'prints a deposit transaction to the console' do
-    statement = Statement.new
-    fake_deposit = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
-    expect do
-      statement.display([fake_deposit])
-    end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n").to_stdout
-  end
+  context 'withdrawals' do
+    it 'prints a single withdrawal to the console' do
+      statement = Statement.new
+      fake_withdrawal_1 = double(:withdrawal, amount: 500, date: "01-12-2022", credit: nil)
+      expect do
+        statement.display([fake_withdrawal_1])
+      end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 ||  || 500.00 || -500.00\"\n").to_stdout
+    end
 
-  it 'prints two deposit transactions to the console and shows the correct balance' do
-    statement = Statement.new
-    fake_deposit_1 = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
-    fake_deposit_2 = double(:deposit, amount: 800, date: "02-12-2022", credit: 'true')
-    expect do
-      statement.display([fake_deposit_1, fake_deposit_2])
-    end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n\"02-12-2022 || 800.00 ||  || 1300.00\"\n").to_stdout
+    it 'prints two withdrawals to the console & displays the correct balance' do
+      statement = Statement.new
+      fake_withdrawal_1 = double(:withdrawal, amount: 500, date: "01-12-2022", credit: nil)
+      fake_withdrawal_2 = double(:withdrawal, amount: 1000, date: "03-12-2022", credit: nil)
+      expect do
+        statement.display([fake_withdrawal_1, fake_withdrawal_2])
+      end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 ||  || 500.00 || -500.00\"\n\"03-12-2022 ||  || 1000.00 || -1500.00\"\n").to_stdout
+    end
   end
 end

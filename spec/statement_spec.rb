c1cbdca8d8742e0087864f3cf5ab13262e1a19a8
Kayleigh Watts <kwatts949@gmail.com>
2022-12-12 14:56:43 +0000

    Add passing tests for statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,8 +1,27 @@
 require 'statement'
 
 RSpec.describe 'Statement' do
-  xit 'prints the header to the console' do
+  it 'prints an empty statement to the console' do
     statement = Statement.new
-    expect(statement.display([])).to eq "date || credit || debit || balance"
+    expect do
+      statement.display([])
+    end.to output("\"date || credit || debit || balance\"\n\"  ||  ||  || 0.00\"\n").to_stdout
+  end
+
+  it 'prints a deposit transaction to the console' do
+    statement = Statement.new
+    fake_deposit = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
+    expect do
+      statement.display([fake_deposit])
+    end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n").to_stdout
+  end
+
+  it 'prints two deposit transactions to the console and shows the correct balance' do
+    statement = Statement.new
+    fake_deposit_1 = double(:deposit, amount: 500, date: "01-12-2022", credit: 'true')
+    fake_deposit_2 = double(:deposit, amount: 800, date: "02-12-2022", credit: 'true')
+    expect do
+      statement.display([fake_deposit_1, fake_deposit_2])
+    end.to output("\"date || credit || debit || balance\"\n\"01-12-2022 || 500.00 ||  || 500.00\"\n\"02-12-2022 || 800.00 ||  || 1300.00\"\n").to_stdout
   end
 end
